4af1f22ca07f75213599a451d9b2be53fa220aba
Michael Gardner <shellshocked2003@gmail.com>
2018-09-05 15:30:47 -0700

    Set up simple unit tests using Catch. Also added Trompeloeil for mock construction for more involved unit testing

--- test/common_tests.cpp
+++ test/common_tests.cpp
@@ -1,11 +1,3 @@
-#if defined(_WIN32) || defined(_WIN62)
-    #include <direct.h>
-#else
-    #include <unistd.h>
-    #include <sys/types.h>
-    #include <sys/stat.h>
-#endif
-#include <cstdio>
 #include <QApplication>
 #include <QDir>
 #include <QString>
@@ -18,13 +10,6 @@ TEST_CASE("Test SimCenterAppWidget copy file and path functions","[AppWidget]") 
   char * argv[] = {(char *)"test"};
   QApplication app(argc, argv);
   SimCenterAppWidget app_widget;
-
-  // Remove directories is they still exist from previous runs, then create them again
-  std::remove("./test_dir/test_copy.txt");
-  rmdir("./test_dir");
-  rmdir("./another_test_dir");
-  mkdir("./test_dir", S_IRWXU);
-  mkdir("./another_test_dir", S_IRWXU);  
   
   SECTION("Copy input path") {
     REQUIRE(app_widget.copyPath("./does_not_exist", "./test_dir", false) == false);
