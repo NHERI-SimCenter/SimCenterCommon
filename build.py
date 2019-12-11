7e049e2497ac580f3a99994ef693ee7680cb74dc
Wael Elhaddad <elhaddad@berkeley.edu>
2019-12-10 18:40:43 -0800

    Fixing Conan builds

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from cpt.packager import ConanMultiPackager
+import copy
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
@@ -9,6 +10,7 @@ if __name__ == "__main__":
     for settings, options, env_vars, build_requires, reference in builder.items:
         options['SimCenterCommonQt:MDOFwithQt3D'] = False
         buildsWithOptions.append([settings, options, env_vars, build_requires, reference])
+        options = copy.deepcopy(options)
         options['SimCenterCommonQt:MDOFwithQt3D'] = True
         buildsWithOptions.append([settings, options, env_vars, build_requires, reference])
     builder.builds = buildsWithOptions
