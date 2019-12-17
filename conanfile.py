611341c39323d66219151a8fb427c47e95d58b01
Wael Elhaddad <elhaddad@berkeley.edu>
2019-12-17 15:43:15 -0800

    Updating version

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ import os
 
 class CommonConan(ConanFile):
     name = "SimCenterCommonQt"
-    version = "0.0.2"
+    version = "0.0.3"
     license = "BSD"
     author = "Wael Elhaddad (elhaddad@berkeley.edu)"
     url = ""
