84b4c0bbe6d79ec2fa60535edede9027202ad8f4
yisangriB <yisangri@berkeley.edu>
2022-09-02 19:47:16 -0700

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit 9cc5c59a7a7a6531b3942071883d209e0b4c0269, reversing
    changes made to 8d878dd38f9255c06d6537ffdc07ba07c37217ba.

--- Workflow/WORKFLOW/WorkflowCLI.h
+++ Workflow/WORKFLOW/WorkflowCLI.h
@@ -1,7 +1,3 @@
-#include <unordered_map>
-#include <string>
-
-class QVariant;
 class MainWindowWorkflowApp;
 class WorkflowAppWidget;
 
@@ -11,30 +7,11 @@ public:
     WorkflowCLI(MainWindowWorkflowApp *windowApp, WorkflowAppWidget* inputApp);
     ~WorkflowCLI();
 
-    int parseAndRun(int argc, char **argv);
-    int runLocal(void);
+  int parseAndRun(int argc, char **argv);
+  int runLocal(void);
 
 private:
-    int configureSimCenterApps(int argc, char **argv);
-    void resetConfiguration(void);
-
-    bool reset_config = true;
-    MainWindowWorkflowApp* window;
-    WorkflowAppWidget* inputApp;
-
-    // Keys stored in "Common" QSettings
-    std::unordered_map<std::string, QVariant*> common_options {
-      {"pythonExePath", nullptr}
-    };
-
-    // Keys stored in application's QSettings
-    std::unordered_map<std::string, QVariant*> app_options {
-      {"localWorkDir",  nullptr},
-      {"remoteWorkDir", nullptr},
-      {"customAppDir",  nullptr},
-      {"openseesPath",  nullptr},
-      {"dakotaPath",    nullptr},
-      {"appDir",        nullptr}
-    };
+  MainWindowWorkflowApp* window;
+  WorkflowAppWidget* inputApp;
 };
 
