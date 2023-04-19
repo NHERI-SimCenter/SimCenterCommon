5c5e0a4929c33721b8adf9414ce7be7b2a8e34e2
yisangriB <yisangri@berkeley.edu>
2023-04-18 19:42:28 -0700

    sy - minor updates

--- Common/SC_DoubleLineEdit.cpp
+++ Common/SC_DoubleLineEdit.cpp
@@ -54,6 +54,12 @@ SC_DoubleLineEdit::SC_DoubleLineEdit(QString theKey, double initValue, QString t
   this->setText(QString::number(initValue));
 }
 
+SC_DoubleLineEdit::SC_DoubleLineEdit(QString theKey)
+  :QLineEdit()
+{
+  key = theKey;
+}
+
 SC_DoubleLineEdit::~SC_DoubleLineEdit()
 {
 
