c71b96ac8988f1e5edc1f10ec4c71ea4c6c0c2a9
fmckenna <fmckenna@berkeley.edu>
2024-08-06 08:18:16 -0700

    fmk - adding debug to login

--- Workflow/EXECUTION/TapisV3.cpp
+++ Workflow/EXECUTION/TapisV3.cpp
@@ -75,6 +75,114 @@ static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
     return written;
 }
 
+
+struct data {
+  char trace_ascii; /* 1 or 0 */
+};
+ 
+static
+void dump(const char *text,
+          FILE *stream, unsigned char *ptr, size_t size,
+          char nohex)
+{
+  size_t i;
+  size_t c;
+ 
+  unsigned int width = 0x10;
+ 
+  if(nohex)
+    /* without the hex output, we can fit more on screen */
+    width = 0x40;
+ 
+  fprintf(stream, "%s, %10.10lu bytes (0x%8.8lx)\n",
+          text, (unsigned long)size, (unsigned long)size);
+ 
+  for(i = 0; i<size; i += width) {
+ 
+    fprintf(stream, "%4.4lx: ", (unsigned long)i);
+ 
+    if(!nohex) {
+      /* hex not disabled, show it */
+      for(c = 0; c < width; c++)
+        if(i + c < size)
+          fprintf(stream, "%02x ", ptr[i + c]);
+        else
+          fputs("   ", stream);
+    }
+ 
+    for(c = 0; (c < width) && (i + c < size); c++) {
+      /* check for 0D0A; if found, skip past and start a new line of output */
+      if(nohex && (i + c + 1 < size) && ptr[i + c] == 0x0D &&
+         ptr[i + c + 1] == 0x0A) {
+        i += (c + 2 - width);
+        break;
+      }
+      fprintf(stream, "%c",
+              (ptr[i + c] >= 0x20) && (ptr[i + c]<0x80)?ptr[i + c]:'.');
+      /* check again for 0D0A, to avoid an extra \n if it's at width */
+      if(nohex && (i + c + 2 < size) && ptr[i + c + 1] == 0x0D &&
+         ptr[i + c + 2] == 0x0A) {
+        i += (c + 3 - width);
+        break;
+      }
+    }
+    fputc('\n', stream); /* newline */
+  }
+  fflush(stream);
+}
+
+static char theBlahFile='a';
+
+static
+int my_trace(CURL *handle, curl_infotype type,
+             char *data, size_t size,
+             void *userp)
+{
+  struct data *config = (struct data *)userp;
+  const char *text;
+  (void)handle; /* prevent compiler warning */
+ 
+  switch(type) {
+  case CURLINFO_TEXT:
+    fprintf(stderr, "== Info: %s", data);
+    return 0;
+  case CURLINFO_HEADER_OUT:
+    text = "=> Send header";
+    break;
+  case CURLINFO_DATA_OUT:
+    text = "=> Send data";
+    break;
+  case CURLINFO_SSL_DATA_OUT:
+    text = "=> Send SSL data";
+    break;
+  case CURLINFO_HEADER_IN:
+    text = "<= Recv header";
+    break;
+  case CURLINFO_DATA_IN:
+    text = "<= Recv data";
+    break;
+  case CURLINFO_SSL_DATA_IN:
+    text = "<= Recv SSL data";
+    break;
+  default: /* in case a new one is introduced to shock us */
+    return 0;
+  }
+
+  QString debugFilePath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation)
+    + QDir::separator() + QCoreApplication::applicationName() + QDir::separator()
+    + QString(theBlahFile) + QString(".txt");
+  
+  QByteArray byteArray = debugFilePath.toUtf8();
+  const char* cString = byteArray.data();
+  theBlahFile++;
+  //FILE *fileOut = fopen("Users/fmckenna/Documents/quoFEM/curl.txt","a");
+  FILE *fileOut = fopen(cString,"a");  
+  dump(text, fileOut, (unsigned char *)data, size, config->trace_ascii);
+  fclose(fileOut);
+  return 0;
+}
+
+
 TapisV3::TapisV3(QString &_tenant, QString &_storage, QString *appDir, QObject *parent)
     :RemoteService(parent), tenant(_tenant), storage(_storage), loggedInFlag(false), slotNeededLocally(false)
 {
@@ -202,6 +310,16 @@ TapisV3::login(QString uname, QString upassword)
     username = uname;
     password = upassword;
 
+    //
+    // add debugging of curl calls
+    //
+    
+    struct data config;
+    config.trace_ascii = 1;
+    curl_easy_setopt(hnd, CURLOPT_DEBUGFUNCTION, my_trace);
+    curl_easy_setopt(hnd, CURLOPT_DEBUGDATA, &config);
+    curl_easy_setopt(hnd, CURLOPT_VERBOSE, 1L);
+    
     url = tenantURL + QString("v3/oauth2/tokens");
     user_passwd =QString("{\"username\": \"%1\", \"password\": \"%2\", \"grant_type\": \"password\" }").arg(username, password);
     slist1 = NULL;
@@ -1217,6 +1335,8 @@ TapisV3::deleteJob(const QString &jobID, const QStringList &dirToRemove)
     }
     return false;
 }
+
+
 
 
 bool
