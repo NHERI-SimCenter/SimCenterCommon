ca1a5ebd89815e2cff042797291bbc3a9605b413
fmckenna <fmckenna@berkeley.edu>
2025-07-31 12:54:19 -0700

    fmk - correcting backendlocation for stampede3

--- Common/SimCenterPreferences.cpp
+++ Common/SimCenterPreferences.cpp
@@ -979,7 +979,7 @@ SimCenterPreferences::getRemoteAppDir(void) {
 	
 	// if not set, use default & set default as application directory
 	if (!remoteBackendDirVariant.isValid()) {
-	  QString remoteBackendDirLocation = QString("/work2/00477/tg457427/stampede3/SimCenterBackendApplications/v25.05.14");
+	  QString remoteBackendDirLocation = QString("/work2/00477/tg457427/stampede3/SimCenterBackendApplications/v25.05.16");
 
 	  QString appName = QCoreApplication::applicationName();
 	  if (appName == QString("WE-UQ") || appName == QString("HydroUQ"))
@@ -1145,7 +1145,7 @@ QString
 SimCenterPreferences::getDefaultRemoteAppDir(void) {
 
   QString appName = QCoreApplication::applicationName();  
-  QString remoteBackendDirLocation = QString("/work2/00477/tg457427/stampede3/SimCenterBackendApplications/v25.05.14");
+  QString remoteBackendDirLocation = QString("/work2/00477/tg457427/stampede3/SimCenterBackendApplications/v25.05.16");
   if (appName == QString("WE-UQ") || appName == QString("HydroUQ"))
     remoteBackendDirLocation = QString("/work2/00477/tg457427/frontera/SimCenterBackendApplications/v4.6.0");  
 
